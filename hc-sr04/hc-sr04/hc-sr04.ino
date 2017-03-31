277ca18d3761197446f64073039cca561c0f9951
Martin Sollie <martin.lysvand.sollie@gmail.com>
2017-03-31 12:07:46 +0200

    Fix sonar I2C code

--- hc-sr04/hc-sr04/hc-sr04.ino
+++ hc-sr04/hc-sr04/hc-sr04.ino
@@ -4,23 +4,27 @@
 #define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     7  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
+#define SLAVE_ADDRESS 0x55
 
 NewPing sonar = NewPing(6, 7, MAX_DISTANCE);
 
-int heightCm = 0; 
+int heightCm = 0;
+byte data[2];
 
 void setup() {
-  Wire.begin(6);
-  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {
   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   heightCm = sonar.ping_cm();
-  Serial.print("Ping: ");
-  Serial.print(heightCm); 
-  Serial.println("cm");
+}
 
-  Wire.write(sonar.ping_cm());
+void requestEvent() {
+  byte l = heightCm & 0xff;
+  byte h = (heightCm & 0xff00) >> 8;
+  data[0] = h;
+  data[1] = l;  
+  Wire.write(data, 2);  
 }
