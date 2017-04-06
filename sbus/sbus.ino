7828a3c90968258a91adc14fc1650fe4374259b3
Martin Sollie <martin.lysvand.sollie@gmail.com>
2017-04-06 14:04:16 +0200

    Add safety features for rc loss

--- sbus/sbus.ino
+++ sbus/sbus.ino
@@ -1,10 +1,16 @@
 #include <SBUS.h>
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x50
-#define NUM_CHANNELS 7
+#define TIMEOUT_MS 200 
+
+/*
+ * 7 channels: 4 for sticks, 1 for arm, 2 for mode switches
+ * 1 byte for failsafe status (bit 0) and timeout status (bit 1)
+ */
+#define NUM_BYTES 15
 int i = 0;
-byte ch[2*NUM_CHANNELS];
-int chan[NUM_CHANNELS];
+byte ch[NUM_BYTES];
+int chan[7];
 SBUS sBus(Serial);
 
 void setup() {
@@ -22,12 +28,16 @@ ISR(TIMER2_COMPA_vect)
 void loop(){}
 
 void requestEvent() {
-  for (i = 0; i < NUM_CHANNELS; i++) {
+  for (i = 0; i < 7; i++) {
     chan[i] = sBus.getChannel(i+1);
     byte l = chan[i] & 0xff;
     byte h = (chan[i] & 0xff00) >> 8;
     ch[2*i] = h;
     ch[2*i+1] = l;
-  }    
-  Wire.write(ch, 2*NUM_CHANNELS);  
+  }
+  ch[14] = 0xff & sBus.getFailsafeStatus(); // 1 = failsafe, 0 = no failsafe
+  if(millis() - sBus.getLastTime() > TIMEOUT_MS){
+    ch[14] |= 0b10;
+  }
+  Wire.write(ch, NUM_BYTES);  
 }
